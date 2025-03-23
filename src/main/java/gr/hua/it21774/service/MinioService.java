5d002f950c3201971853c567d1a58158703a90b7
Kevin Stana <it21774@hua.gr>
2025-03-23 17:24:17 +0200

    Fixes

--- src/main/java/gr/hua/it21774/service/MinioService.java
+++ src/main/java/gr/hua/it21774/service/MinioService.java
@@ -59,13 +59,18 @@ public class MinioService {
     }
 
     public String getSignedUrl(String bucketName, String folderName, String fileName) throws Exception {
-        return minioClient.getPresignedObjectUrl(
+        String signedUrl = minioClient.getPresignedObjectUrl(
                 GetPresignedObjectUrlArgs.builder()
                         .method(Method.GET)
                         .bucket(bucketName)
                         .object("/" + folderName + "/" + fileName)
                         .expiry(60, TimeUnit.SECONDS)
                         .build());
+
+        signedUrl = signedUrl.replace("minio:9000", "localhost:9000");
+
+        return signedUrl;
+
     }
 
     public void uploadMultipleFiles(List<MultipartFile> files, String bucketName, String folderName) throws Exception {
